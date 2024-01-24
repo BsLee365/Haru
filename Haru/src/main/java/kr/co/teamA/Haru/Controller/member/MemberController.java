b66dc7c11f5b2acffc5e2954632c8a9f0c23a13e
uijk989 <uijk989@naver.com>
2024-01-25 00:18:09 +0900

    11

--- Haru/src/main/java/kr/co/teamA/Haru/Controller/member/MemberController.java
+++ Haru/src/main/java/kr/co/teamA/Haru/Controller/member/MemberController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/memberrr")
+@RequestMapping("/member")
 public class MemberController {
 
     @Autowired

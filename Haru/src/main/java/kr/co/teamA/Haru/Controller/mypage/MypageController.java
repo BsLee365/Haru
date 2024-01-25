d66ff7a51edcf4584bd182c151c48d0e0a14c582
uijk989 <uijk989@naver.com>
2024-01-25 16:03:18 +0900

    머지 수정함

--- Haru/src/main/java/kr/co/teamA/Haru/Controller/mypage/MypageController.java
+++ Haru/src/main/java/kr/co/teamA/Haru/Controller/mypage/MypageController.java
@@ -1,18 +1,35 @@
 package kr.co.teamA.Haru.Controller.mypage;
 
 import kr.co.teamA.Haru.Service.myPage.MyPageService;
-import org.springframework.stereotype.Controller;
+
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class MypageController {
 
     MyPageService myPageService;
 
-    // 마이페이지 데이터
+    public MypageController(MyPageService myPageService) {
+        this.myPageService = myPageService;
+    }
+
     @PostMapping("/myPageData")
     public String myPageData(@RequestBody String token) {
         return myPageService.myPageData(token);
+    }
+
+    @PostMapping("/getMyPageData")
+    public Map<String, Object> getMyPageData(@RequestParam Map<String, Object> data) {
+        String userId = (String) data.get("userId");
+        System.out.println("서비스 들어감");
+        Map<String, Object> myPageData = myPageService.getMyPageData(userId);
+        System.out.println("서비스 반환");
+
+        return myPageData;
     }
 }

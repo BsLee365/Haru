f555451d12c5ced035ea929f8f6685833e8c67fd
WoodenBat <145077555+WoodenBat@users.noreply.github.com>
2024-01-26 12:06:23 +0900

    홈화면 수치 네개 다 구현, 회원정보 수정 구현, 피드 버그 수정 등등

--- Haru/src/main/java/kr/co/teamA/Haru/Service/home/HomeService.java
+++ Haru/src/main/java/kr/co/teamA/Haru/Service/home/HomeService.java
@@ -1,12 +1,15 @@
 package kr.co.teamA.Haru.Service.home;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Service;
 
 import kr.co.teamA.Haru.Entity.Member;
+import kr.co.teamA.Haru.Entity.StressData;
 import kr.co.teamA.Haru.Repository.MemberRepository;
+import kr.co.teamA.Haru.Repository.StressRepository;
 import kr.co.teamA.Haru.Repository.feed.FeedLikeRepository;
 import kr.co.teamA.Haru.Repository.feed.FeedRepository;
 
@@ -16,12 +19,14 @@ public class HomeService {
     public final MemberRepository memberRepository;
     public final FeedRepository feedRepository;
     public final FeedLikeRepository feedLikeRepository;
+    public final StressRepository stressRepository;
 
     public HomeService(MemberRepository memberRepository, FeedRepository feedRepository,
-            FeedLikeRepository feedLikeRepository) {
+            FeedLikeRepository feedLikeRepository, StressRepository stressRepository) {
         this.memberRepository = memberRepository;
         this.feedRepository = feedRepository;
         this.feedLikeRepository = feedLikeRepository;
+        this.stressRepository = stressRepository;
     }
 
     public Map<String, Object> getHomeData() {
@@ -30,10 +35,11 @@ public class HomeService {
         int memberCount = memberRepository.countByUserIdIsNotNull();
         int feedCount = feedRepository.countByFeedNumIsNotNull();
         int likeCount = feedLikeRepository.countByLikeNumIsNotNull();
-
+        List<StressData> stressList = stressRepository.findAll();
         homeData.put("memberCount", memberCount);
         homeData.put("feedCount", feedCount);
         homeData.put("likeCount", likeCount);
+        homeData.put("stressList", stressList);
 
         return homeData;
     }
